d7d33f7cb81a22ef26eb45dcef7af3b47ca15d49
CAMHub PublishBot <octravis@us.ibm.com>
2019-10-02 23:07:40 +0000

    Automated CAMHub::publish on 2019-10-02 23:07:40 UTC

--- vmware/terraform/output.tf
+++ vmware/terraform/output.tf
@@ -1,3 +1,7 @@
+resource "random_id" "clusterid" {
+  byte_length = "2"
+}
+
 output "ibm_cloud_private_admin_url" {
   value = "https://icp-console.${var.ocp_infra_ip}.nip.io"
 }
@@ -8,4 +12,16 @@ output "ibm_cloud_private_admin_user" {
 
 output "ibm_cloud_private_admin_password" {
   value = "${var.icp_admin_password}"
+}
+
+output "ibm_cloud_private_boot_ip" {
+  value = "${var.ocp_master_ip}"
+}
+
+output "registry_config_do_name"{
+	value = "${var.icp_cluster_name}${random_id.clusterid.hex}RegistryConfig" 
+}
+
+output "icp_install_dir"{
+  value = "${camc_scriptpackage.get_home_dir.result["stdout"]}/ibm-cloud-private-rhos-${var.icp_version}/cluster"
 }
